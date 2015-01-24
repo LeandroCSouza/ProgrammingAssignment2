4ad44a024f3f25dc05ea47c4064d636b426713e2
LeandroCSouza <leandrocostsou@hotmail.com>
2015-01-24 18:45:30 -0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Funtion to cache the matrix and make its invertion
 
 makeCacheMatrix <- function(x = matrix()) {
   m<-NULL
